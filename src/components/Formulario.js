de368dd921c48e4287f5713786de97863a320b1c
Ana Hernandez <anaceciliah95@gmail.com>
2020-04-13 00:53:07 -0400

    validacion gasto

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,15 +1,43 @@
 import React, {useState} from 'react';
+import Error from './Error';
 
 const Formulario = () => {
+
+    const [nombre, guardarNombre] = useState("");
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, actualizarError] = useState(false);
+
+    //Agregar gasto
+    const agregarGasto = e => {
+        e.preventDefault();
+        // Validar
+        if(cantidad < 1 || isNaN(cantidad) || nombre.trim() === ''){
+            actualizarError(true);
+            return;
+        }
+        actualizarError(false);
+
+        // Construir gasto
+
+        // Pasar al componente principal
+
+        // resetear el form
+    }
+
     return ( 
-        <form>
+        <form
+            onSubmit={agregarGasto}
+        >
             <h2>Agrega tus gastos aquí</h2>
+            {error? <Error mensaje="Presupuesto incorrecto"/> : null}
             <div className="campo">
                 <label>Nombre gasto</label>
                 <input
                     type="text"
                     className="u-full-width"
                     placeholder="Ej. Transporte"
+                    value={nombre}
+                    onChange={e => guardarNombre(e.target.value)}
                 />
             </div>
             <div className="campo">
@@ -18,6 +46,8 @@ const Formulario = () => {
                     type="number"
                     className="u-full-width"
                     placeholder="Ej. 300"
+                    value={cantidad}
+                    onChange={e => guardarCantidad(parseInt(e.target.value))}
                 />
             </div>
                 <input
